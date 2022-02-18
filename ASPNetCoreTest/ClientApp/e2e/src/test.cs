7d5c197b59e20047fd853c1978154c0fa42700fb
Jitencoder <61916215+Jitencoder@users.noreply.github.com>
2022-02-18 07:30:50 -0500

    new class updated

--- ASPNetCoreTest/ClientApp/e2e/src/test.cs
+++ ASPNetCoreTest/ClientApp/e2e/src/test.cs
@@ -1,1 +1,19 @@
-Hi this is a test file
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace ASPNetCoreTest
+{
+    public class RestClient
+    { }
+
+}
